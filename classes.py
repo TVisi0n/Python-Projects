802844b6648a5e9ed0e5bd8cc8ffa3a0fb97d478
Tanner Vasquez <126614745+TVisi0n@users.noreply.github.com>
2023-09-26 18:49:52 -0700

    Created classes.py

---
+++ classes.py
@@ -0,0 +1,18 @@
+
+#Created a Vehicle class
+class Vehicles:
+    Make = "Hyundai"
+    Model = "Elantra"
+    Year = 2013
+    Color = "Blue"
+
+#Here are the specs/info on the said vehicle
+class info(Vehicles):
+    Class = "Sedan"
+    Doors = "4 Doors"
+    Engine = "4.1 liter"
+
+#This is the location of where to purchase the car
+class Dealership(Vehicles):
+    Name = "Ron Tonkin"
+    Location = "Hillsboro"
