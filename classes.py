78ae73bdd6cf5d3b52dbf5ddb466ae05a4adf615
Tanner Vasquez <126614745+TVisi0n@users.noreply.github.com>
2023-09-27 15:48:32 -0700

    updated classes.py

--- classes.py
+++ classes.py
@@ -3,8 +3,18 @@
 class Vehicles:
     Make = "Hyundai"
     Model = "Elantra"
-    Year = 2013
+    Year = "2013"
     Color = "Blue"
+
+    def enterVehicle(self):
+        enter_make = input("Enter the Make of Vehicle: ")
+        enter_model = input("Enter the Model: ")
+        enter_year = input("Enter the Year: ")
+        enter_color = input("Enter the Color: ")
+        if (enter_make == self.Make and enter_model == self.Model and enter_year == self.Year and enter_color == self.Color):
+            print("This {} {} is in stock!".format(enter_make, enter_model))
+        else:
+            print("This vehicle is not in stock! \nWe are sorry :(")
 
 #Here are the specs/info on the said vehicle
 class info(Vehicles):
@@ -12,7 +22,24 @@ class info(Vehicles):
     Doors = "4 Doors"
     Engine = "4.1 liter"
 
+    def enterVehicle(self):
+        enter_make = input("Enter the Make of Vehicle: ")
+        enter_model = input("Enter the Model: ")
+        enter_year = input("Enter the Year: ")
+        enter_engine = input("Enter the Engine Type: ")
+        if (enter_make == self.Make and enter_model == self.Model and enter_year == self.Year and enter_engine == self.Engine):
+            print("This {} {} is in stock!".format(enter_make, enter_model))
+        else:
+            print("This vehicle is not in stock! \nWe are sorry :(")
+        
 #This is the location of where to purchase the car
 class Dealership(Vehicles):
     Name = "Ron Tonkin"
     Location = "Hillsboro"
+
+
+sales = Vehicles()
+sales.enterVehicle()
+
+dealership = info()
+dealership.enterVehicle()
