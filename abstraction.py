64c13f0440614c7bb2e02dbcabca5277cb54fc57
Tanner Vasquez <126614745+TVisi0n@users.noreply.github.com>
2023-10-02 19:14:41 -0700

    Another update

--- abstraction.py
+++ abstraction.py
@@ -12,18 +12,6 @@ class roll(die):
     def rollAmount(self, amount):
         print('You rolled a {}! Roll again!'.format(amount))
 
-# User's first roll
-#Roll1 = front()
-#Roll1.sideno()
-
-# User's second roll
-#Roll2 = right()
-#Roll2.sideno()
-
-# User's third roll
-#Roll3 = back()
-#Roll3.sideno()
-
 obj = roll()
 obj.yourRoll("1")
 obj.rollAmount("1")
